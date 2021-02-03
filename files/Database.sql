9f556e9d05fbfc5f591db4102801a2b17e7ea154
minatore06 <35301775+minatore06@users.noreply.github.com>
2021-02-03 09:47:34 +0100

    edited database final

--- files/Database.sql
+++ files/Database.sql
@@ -29,7 +29,7 @@ CREATE TABLE `foobar` (
   `nome` varchar(32) DEFAULT NULL,
   `cognome` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
